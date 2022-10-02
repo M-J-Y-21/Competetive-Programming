8746150aa27d868685961b1fd75299d238e74fe7
M-J-Y-21 <matanyakobovich@yahoo.com>
2022-10-02 09:57:18 +0200

    Initial Set of Completed Code Forces Problems

---
+++ TheatreSquare.cpp
@@ -0,0 +1,24 @@
+// diff 1000
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // solution
+    long long int n, m, a;
+    cin >> n >> m >> a;
+
+    long long int squaresL = ceil(m / (double) a);
+    long long int squaresH = ceil(n / (double) a);
+
+    // if (n <= a && m <= a) {
+    //     if (m > a) {
+    //         cout << squaresL << endl;
+    //     } else {
+    //         cout << squaresH << endl;
+    //     }
+    // } else {
+    //     cout << squaresL + squaresH << endl;
+    // }
+    cout << squaresL * squaresH << endl;
+}
