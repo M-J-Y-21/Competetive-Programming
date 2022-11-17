1adab73dbfacad191542f790502a64c609e87a86
M-J-Y-21 <matanyakobovich@yahoo.com>
2022-11-17 20:50:28 +0100

    Added comments to Double Cola (Math)

--- codeforces/1100/DoubleCola.cpp
+++ codeforces/1100/DoubleCola.cpp
@@ -13,11 +13,21 @@ int main() {
 
     int n, i = 0, ans = 0;
     cin >> n;
+
+    /**
+     * We are trying to find essentially the number of rows deep we need to go
+     * into to get to that person this is represented by i.
+     * We n -= 5 * pow(2, i) because we want to reduce our search with the rows 
+     * and we'll continue doing this until we see we've reached the destination row.
+     */
     while (n > 5 * pow(2, i)) {
         n -= 5 * pow(2, i);
         i++;
     }
 
+    /**
+     * Zero indexed thus n-1 and div by pow(2, i) to get the exact index.
+    */
     ans = (n - 1) / pow(2, i);
 
     switch (ans) {
