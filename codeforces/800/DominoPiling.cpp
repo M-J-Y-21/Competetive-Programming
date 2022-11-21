6196e025bfb79b1134a1f17aa7605a4c1a122588
M-J-Y-21 <matanyakobovich@yahoo.com>
2022-11-21 16:39:35 +0100

    Domino Piling

---
+++ codeforces/800/DominoPiling.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /**
+     * @brief Super speed boost
+     */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int m, n, area;
+
+    cin >> m >> n;
+    area = m * n;
+    cout << area / 2 << endl;
+
+    return 0;
+}
